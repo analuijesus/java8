96b6e07c73323a909a4c4bafcf1613c4d398a4dc
Ana de Jesus <ana_dejesus14@outlook.com>
2021-05-25 13:54:49 -0300

    collectors -> utilizando diversos filtros e maps

--- src/StreamTeste.java
+++ src/StreamTeste.java
@@ -1,6 +1,11 @@
+import sun.security.ec.ECKeyPairGenerator;
+import sun.util.resources.cldr.saq.CurrencyNames_saq;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static java.util.Comparator.comparing;
 import static java.util.Comparator.comparingInt;
@@ -20,17 +25,39 @@ public class StreamTeste {
 //        cursos.sort(comparingInt(Curso::getAluno));
 //        cursos.forEach(c -> System.out.println(c.getAluno()));
 
+//        cursos.stream()
+//                .filter(c -> c.getAluno() >= 100)
+//                .map(Curso::getAluno)
+//                //.map(c -> c.getAluno()) -> mesmo map acima
+//                .forEach(System.out::println);
+//
+//        int soma = cursos.stream()
+//                .filter(c -> c.getAluno() >= 100)
+//                .mapToInt(Curso::getAluno)
+//                .sum();
+//        System.out.println(soma);
+
+//        cursos.stream() // cada stream feito é "começado" do zero. como se anterior não tivesse diferenteça utilizando o Collectores.toList isso muda
+//                .filter(c -> c.getAluno() >= 100)
+//                .findAny()
+//                .ifPresent(c -> System.out.println(c.getCurso()));
+//
+//        cursos.stream()
+//                .filter(c -> c.getAluno() >= 100)
+//                .collect(Collectors.toList()); // transforma em uma "nova lista"
+
+//        Map<String, Integer> map = cursos.stream()
+//                .filter(c -> c.getAluno() >= 100)
+//                .collect(Collectors.toMap(Curso::getCurso, Curso::getAluno));
+//
+//        System.out.println(map);
+
         cursos.stream()
                 .filter(c -> c.getAluno() >= 100)
-                .map(Curso::getAluno)
-                //.map(c -> c.getAluno()) -> mesmo map acima
-                .forEach(System.out::println);
+                //.collect(Collectors.toMap(Curso::getCurso, Curso::getAluno))
+                .collect(Collectors.toMap(c -> c.getCurso(), c -> c.getAluno()))
+                .forEach((Curso::getAluno, Curso::getCurso) -> System.out.println("Oi"); // pesquisar como implementar esse código
 
-        int soma = cursos.stream()
-                .filter(c -> c.getAluno() >= 100)
-                .mapToInt(Curso::getAluno)
-                .sum();
-        System.out.println(soma);
     }
 }
 
